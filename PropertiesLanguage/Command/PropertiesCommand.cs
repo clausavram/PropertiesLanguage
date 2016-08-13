1b0ee071b476c0277e5691f337ec22204894019f
clausavram <clausavram@gmail.com>
2016-08-14 01:40:22 +0300

    Added dummy Menu & Toolbar buttons for Theme changing

---
+++ PropertiesLanguage/Command/PropertiesCommand.cs
@@ -0,0 +1,105 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="PropertiesCommand.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace PropertiesLanguage {
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class PropertiesCommand {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int ColorsMenuCmdId = 0x0100;
+        public const int ColorsToolbarCmdId = 0x0101;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("4d66743f-fd61-4607-b6c3-6e30aeea248a");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropertiesCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private PropertiesCommand(Package package) {
+            if (package == null) {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            this.package = package;
+
+            var commandService = ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null) {
+                var menuCommandId = new CommandID(CommandSet, ColorsMenuCmdId);
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandId);
+                commandService.AddCommand(menuItem);
+
+                var toolbarCommanId = new CommandID(CommandSet, ColorsToolbarCmdId);
+                var toolbarItem = new MenuCommand(MenuItemCallback, toolbarCommanId);
+                commandService.AddCommand(toolbarItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static PropertiesCommand Instance {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider {
+            get {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package) {
+            Instance = new PropertiesCommand(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            string message = "Claudiu's command!!!";
+            string title = "PropertiesCommand";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ PropertiesLanguage/Command/PropertiesCommandPackage.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace PropertiesLanguage {
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class PropertiesCommandPackage : Package {
+        public const string PackageGuidString = "aed4961a-4295-4fc6-8557-e7427b6e8d21";
+
+        public PropertiesCommandPackage() {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize() {
+            PropertiesCommand.Initialize(this);
+            base.Initialize();
+        }
+
+        #endregion
+    }
+}
